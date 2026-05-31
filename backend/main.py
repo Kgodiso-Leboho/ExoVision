72e387002ea5e9befee0c6d501475076b08f2daf
Kgodiso-Leboho <kgodisoaustinleboho@gmail.com>
2026-05-31 08:51:33 +0200

    fix: add CORS middleware to allow requests from React frontend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.auth import authRouter
 from contextlib import asynccontextmanager
 from app.util.init_db import create_tables
@@ -11,6 +12,14 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router=authRouter, tags=["auth"], prefix="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
